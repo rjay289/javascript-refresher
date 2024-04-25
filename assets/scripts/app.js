2dc52720e896b716f2725d392191138300392376
rjay289 <167650555+rjay289@users.noreply.github.com>
2024-04-24 22:48:14 -0600

    Initial commit

---
+++ assets/scripts/app.js
@@ -0,0 +1,22 @@
+//#2
+// //importing not default value apiKey here for example (js is case sensitive)
+// import { apiKey } from "./utils.js";
+// console.log(apiKey);
+
+// //importing default value. you assign a name to it here
+// import myName from "./utils.js";
+// console.log(myName);
+
+// //Method 1 of importing multiple values: listing the names of values we want
+// // import { date, text } from "./utils.js";
+// // console.log(date);
+// // console.log(text);
+
+// //Method 2 of importing multiple values: bring the whole file as an object
+// import * as utils from "./utils.js";
+// console.log(utils.date);
+// console.log(utils.text);
+
+// //'as' can be used for aliases
+// import { date as DATE } from "./utils.js";
+// console.log(DATE);
---
+++ assets/scripts/utils.js
@@ -0,0 +1,14 @@
+//#1
+// //export makes it so that this can now be accessed beyond this file
+// //we can export many values declared this way
+// export let apiKey = "radia1234";
+
+// //just exporting a value. we're basically saying the default value
+// //this file should export is this. also only 1 default value per file.
+// export default "radia";
+
+// //example to show how to export multiple values
+// export let date = "idk";
+// export let text = "whatElse";
+
+// //fyi you can export/import functions
